3f3f136baae08faf3392877dfc3e8b6795a908fc
EC2 Default User <ec2-user@ip-172-31-8-232.ap-northeast-1.compute.internal>
2023-01-22 08:39:09 +0000

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,51 @@
 class BooksController < ApplicationController
-  def new
+# 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    if @book.save
+    # 4. トップ画面へリダイレクト
+    flash[:notice] = "successfully"
+    redirect_to "/books/#{@book.id}"
+    else
+    @books = Book.all
+    render :index
+    end
   end
-
   def index
+    @book = Book.new
+    @books = Book.all
+
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト
+  end
+
+    private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
